878fc4e1016771dab3cc5cbabe128a29c79c79f4
GyuSeok Nam <52943549+nks0614@users.noreply.github.com>
2020-12-06 11:40:58 +0900

    Revert "[UPDATE] Test My Library"
    This reverts commit a7fa7dd1490c13157a655f3d189bc876e174d255.

--- app/src/main/java/com/project/kotlin/Fragment/FirstFragment.kt
+++ app/src/main/java/com/project/kotlin/Fragment/FirstFragment.kt
@@ -6,10 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.project.kotlin.R
-import com.project.kotlin.SecondActivity
-import com.project.simplecode.spfIntentNoFin
-import com.project.simplecode.spfToastShort
-import kotlinx.android.synthetic.main.fragment_first.*
 
 class FirstFragment : Fragment() {
 
@@ -19,16 +15,5 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_first, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-
-
-        moveBtn.setOnClickListener {
-            spfToastShort("이동합니다")
-            spfIntentNoFin(SecondActivity::class.java)
-        }
     }
 }
