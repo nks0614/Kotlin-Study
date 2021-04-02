dc9c58466e31636ad3878a27620883c47d4b2030
GyuSeok Nam <52943549+nks0614@users.noreply.github.com>
2021-04-02 14:41:57 +0900

    [ADD] Skeleton Library

--- app/src/main/java/com/project/kotlin/fragment/SecondFragment.kt
+++ app/src/main/java/com/project/kotlin/fragment/SecondFragment.kt
@@ -5,12 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.ethanhua.skeleton.Skeleton
+import com.ethanhua.skeleton.SkeletonScreen
 import com.project.kotlin.R
 import io.reactivex.rxjava3.annotations.NonNull
 import io.reactivex.rxjava3.core.Observable
+import kotlinx.android.synthetic.main.fragment_second.*
 
 
 class SecondFragment : Fragment() {
+
+    lateinit var skeletonScreen : SkeletonScreen
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,12 +28,10 @@ class SecondFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-//        val source = Observable.create { emitter: ObservableEmitter<String?> ->
-//            emitter.onNext("Hello")
-//            emitter.onNext("Yena")
-//            emitter.onComplete()
-//        }
-//        source.subscribe { s: String? -> Log.d("test", s) }
-   
+        skeletonScreen = Skeleton.bind(testView)
+            .load(R.layout.fragment_second)
+            .duration(1000)
+            .color(R.color.shimmer_color)
+            .show()
     }
 }
